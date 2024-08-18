d7ec5f77c504c0aff6226f82285ffb41c1c12c9d
David Nieto <xampycod@hotmail.com>
2024-08-18 14:10:54 +0000

    Contador final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import SecondsCounter from './SecondsCounter';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [seconds, setSeconds] = useState(0);
+    const [inputValue, setInputValue] = useState(0);
+    const [isCountingDown, setIsCountingDown] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(prevSeconds => isCountingDown ? Math.max(prevSeconds - 1, 0) : prevSeconds + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [isCountingDown]);
+
+    const handleAddTime = () => {
+        setSeconds(prevSeconds => prevSeconds + parseInt(inputValue, 10));
+    };
+
+    const handleSubtractTime = () => {
+        setSeconds(prevSeconds => Math.max(prevSeconds - parseInt(inputValue, 10), 0));
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const toggleCountDirection = () => {
+        setIsCountingDown(prevState => !prevState);
+    };
+
+    return (
+        <div>
+            <SecondsCounter seconds={seconds} />
+            <div className="controls d-flex flex-column align-items-center mt-3">
+                <div className="mb-3 p-5 mx-auto">
+                    <label htmlFor="formGroupExampleInput" className="form-label">Elige tu tiempo aquí</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="addingTime" 
+                        placeholder="Elige tu tiempo" 
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button onClick={handleAddTime} className='btn btn-outline-dark mt-2 '>Add Time</button>
+                <button onClick={handleSubtractTime} className='btn btn-outline-dark mt-2'>Subtract Time</button>
+                <button onClick={toggleCountDirection} className='btn btn-outline-dark mt-2'>
+                    {isCountingDown ? 'Switch to Count Up' : 'Switch to Count Down'}
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
